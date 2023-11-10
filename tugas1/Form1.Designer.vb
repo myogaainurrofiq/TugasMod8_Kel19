8c628c8652b243d3a1665668afd47c0fda2a1344
myogaainurrofiq <m.yogaainurrofiq25@gmail.com>
2023-11-10 23:23:09 +0700

    kelar

---
+++ tugas1/Form1.Designer.vb
@@ -0,0 +1,369 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        TextBox2 = New TextBox()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        BackgroundWorker1 = New ComponentModel.BackgroundWorker()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(356, 66)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 35)
+        Label1.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(646, 30)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 35)
+        Label2.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(132, 219)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 35)
+        Label3.TabIndex = 2
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.Location = New Point(131, 231)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(0, 35)
+        Label4.TabIndex = 3
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label5.Location = New Point(132, 266)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 35)
+        Label5.TabIndex = 4
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Label6.Location = New Point(292, 22)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(239, 46)
+        Label6.TabIndex = 5
+        Label6.Text = "KELOMPOK 19"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Label7.Location = New Point(107, 160)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(49, 23)
+        Label7.TabIndex = 6
+        Label7.Text = "Suhu"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox1.Location = New Point(195, 153)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 30)
+        TextBox1.TabIndex = 7
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button1.Location = New Point(381, 153)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 8
+        Button1.Text = "C to F"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button2.Location = New Point(505, 153)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 9
+        Button2.Text = "C to R"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button3.Location = New Point(627, 153)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 10
+        Button3.Text = "C to K"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button4.Location = New Point(381, 201)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 11
+        Button4.Text = "F to R"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button5.Location = New Point(505, 200)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(94, 29)
+        Button5.TabIndex = 12
+        Button5.Text = "F to K"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button6.Location = New Point(627, 200)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(94, 29)
+        Button6.TabIndex = 13
+        Button6.Text = "F to C"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button7.Location = New Point(381, 250)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(94, 29)
+        Button7.TabIndex = 14
+        Button7.Text = "K to C"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button8.Location = New Point(505, 249)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(94, 29)
+        Button8.TabIndex = 15
+        Button8.Text = "K to F"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button9.Location = New Point(627, 250)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(94, 29)
+        Button9.TabIndex = 16
+        Button9.Text = "K to R"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button10.Location = New Point(627, 297)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(94, 29)
+        Button10.TabIndex = 19
+        Button10.Text = "R to F"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button11.Location = New Point(505, 298)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(94, 29)
+        Button11.TabIndex = 18
+        Button11.Text = "R to K"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Button12.Location = New Point(381, 297)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(94, 29)
+        Button12.TabIndex = 17
+        Button12.Text = "R to C"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox2.Location = New Point(195, 200)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 30)
+        TextBox2.TabIndex = 20
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Label8.Location = New Point(107, 206)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(46, 23)
+        Label8.TabIndex = 21
+        Label8.Text = "Hasil"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Label9.Location = New Point(486, 78)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(50, 23)
+        Label9.TabIndex = 22
+        Label9.Text = "Agha"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Label10.Location = New Point(272, 78)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(47, 23)
+        Label10.TabIndex = 23
+        Label10.Text = "Yoga"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Label11.Location = New Point(346, 78)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(42, 23)
+        Label11.TabIndex = 24
+        Label11.Text = "Tyas"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        Label12.Location = New Point(406, 78)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(58, 23)
+        Label12.TabIndex = 25
+        Label12.Text = "Daniel"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(TextBox2)
+        Controls.Add(Button10)
+        Controls.Add(Button11)
+        Controls.Add(Button12)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ tugas1/Form1.vb
@@ -0,0 +1,237 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_MouseHover(sender As Object, e As EventArgs) Handles Button1.MouseHover
+        Button1.BackColor = Color.Blue
+    End Sub
+
+    Private Sub Button1_MouseLeave(sender As Object, e As EventArgs) Handles Button1.MouseLeave
+        Button1.BackColor = Color.White
+    End Sub
+
+    Private Sub Button4_MouseHover(sender As Object, e As EventArgs) Handles Button4.MouseHover
+        Button4.BackColor = Color.Blue
+    End Sub
+
+    Private Sub Button4_MouseLeave(sender As Object, e As EventArgs) Handles Button4.MouseLeave
+        Button4.BackColor = Color.White
+    End Sub
+
+    Private Sub Button7_MouseHover(sender As Object, e As EventArgs) Handles Button7.MouseHover
+        Button7.BackColor = Color.Blue
+    End Sub
+
+    Private Sub Button7_MouseLeave(sender As Object, e As EventArgs) Handles Button7.MouseLeave
+        Button7.BackColor = Color.White
+    End Sub
+
+    Private Sub Button12_MouseHover(sender As Object, e As EventArgs) Handles Button12.MouseHover
+        Button12.BackColor = Color.Blue
+    End Sub
+
+    Private Sub Button12_MouseLeave(sender As Object, e As EventArgs) Handles Button12.MouseLeave
+        Button12.BackColor = Color.White
+    End Sub
+
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim c As Double
+
+        c = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.cToF(c)
+
+        Label8.Text = "Fahrenheit"
+
+    End Sub
+
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim c As Double
+
+        c = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.cToR(c)
+
+        Label8.Text = "Reamur"
+
+    End Sub
+
+    Private Sub button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim c As Double
+
+        c = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.cToK(c)
+
+        Label8.Text = "Kelvin"
+
+    End Sub
+
+    Private Sub button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim f As Double
+
+        f = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.fToR(f)
+
+        Label8.Text = "Reamur"
+
+    End Sub
+
+    Private Sub button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim f As Double
+
+        f = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.fToK(f)
+
+        Label8.Text = "Kelvin"
+
+    End Sub
+
+    Private Sub button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim f As Double
+
+        f = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.fToC(f)
+
+        Label8.Text = "Celcius"
+
+    End Sub
+
+    Private Sub button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim k As Double
+
+        k = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.kToC(k)
+
+        Label8.Text = "Celcius"
+
+    End Sub
+
+    Private Sub button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim k As Double
+
+        k = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.kToF(k)
+
+        Label8.Text = "Fahrenheit"
+
+    End Sub
+
+    Private Sub button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim k As Double
+
+        k = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.kToR(k)
+
+        Label8.Text = "Reamur"
+
+    End Sub
+
+    Private Sub button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim r As Double
+
+        r = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.rToF(r)
+
+        Label8.Text = "Fahrenheit"
+
+    End Sub
+
+    Private Sub button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim r As Double
+
+        r = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.rToK(r)
+
+        Label8.Text = "Kelvin"
+
+    End Sub
+
+    Private Sub button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+
+        Dim objConv As New TempConv.TempConvComp
+
+        Dim r As Double
+
+        r = CDbl(TextBox1.Text)
+
+        TextBox2.Text = objConv.rToC(r)
+
+        Label8.Text = "Celcius"
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+    End Sub
+End Class
